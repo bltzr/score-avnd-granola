74f1c41ddb7327311638d24a02f4b74ab07849e3
Pia Baltazar <pia.baltazar.pro@gmail.com>
2026-07-13 03:48:55 -0400

    Relabel port chips and show values as percentages (forwarded from update)
    pos/dur/jitter chips now read "position", "+/-deviation", "duration",
    "duration +/-deviation", displaying their 0..1 values as rounded percents.

--- Granola/GranolaUi.hpp
+++ Granola/GranolaUi.hpp
@@ -313,7 +313,7 @@ struct WindowModeItem
 template <typename T>
 struct PortDotValue
 {
-  static constexpr double width() { return 88.; }
+  static constexpr double width() { return 132.; }
   static constexpr double height() { return 16.; }
 
   std::string_view label;
@@ -323,9 +323,10 @@ struct PortDotValue
   void paint(auto ctx)
   {
     char buf[64];
+    // Float ports are 0..1 normalized -> show as a rounded percentage.
     if constexpr(std::is_floating_point_v<T>)
-      std::snprintf(buf, sizeof(buf), "%.*s %.3f", (int)label.size(), label.data(),
-                    (double)value);
+      std::snprintf(buf, sizeof(buf), "%.*s %.0f%%", (int)label.size(), label.data(),
+                    (double)value * 100.);
     else
       std::snprintf(buf, sizeof(buf), "%.*s", (int)label.size(), label.data());
     ctx.begin_path();
@@ -670,10 +671,14 @@ struct Granola::ui
     halp_meta(background, background_dark)
     halp::custom_control<PortDotValue<std::string>, &ins::sound_folder> sound_folder{
         {.label = "sound"}};
-    halp::custom_control<PortDotValue<float>, &ins::pos> position{{.label = "pos"}};
-    halp::custom_control<PortDotValue<float>, &ins::pos_j> pos_jit{{.label = "posjit"}};
-    halp::custom_control<PortDotValue<float>, &ins::dur> duration{{.label = "dur"}};
-    halp::custom_control<PortDotValue<float>, &ins::dur_j> dur_jit{{.label = "durjit"}};
+    halp::custom_control<PortDotValue<float>, &ins::pos> position{
+        {.label = "position"}};
+    halp::custom_control<PortDotValue<float>, &ins::pos_j> pos_jit{
+        {.label = "+/-deviation"}};
+    halp::custom_control<PortDotValue<float>, &ins::dur> duration{
+        {.label = "duration"}};
+    halp::custom_control<PortDotValue<float>, &ins::dur_j> dur_jit{
+        {.label = "duration +/-deviation"}};
   } ports;
 
   // Drop a folder/file here, or click when empty to open the load dialog.
